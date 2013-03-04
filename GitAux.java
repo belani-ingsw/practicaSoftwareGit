949b84fd629d61b87667ad92ff1ec9883b7fd02d
Kay <j.c.revolution@hotmail.com>
2013-03-04 19:10:13 +0100

    modificaciones alumno 2

--- GitAux.java
+++ GitAux.java
@@ -15,10 +15,14 @@ public class GitAux {
     
     public void metodoAlumno2(){
         System.out.println("metodo 2");
+        System.out.println("MEGAMODIFICACION DEL ALUMNO 2"); // alumno 2
+
     }
     
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
+        //SUPER ULTRA MODIFICACION alumno 2
+        System.out.println("Alumno 2 esta hiperescribiendo.");
     }
 
     public void testGitAux (){
